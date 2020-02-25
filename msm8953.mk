cb9843b1f9677516cf8be615247820c6e6fdd2a3
reynold29 <reynoldclare02@gmail.com>
2020-02-25 17:43:08 +0530

    ysl: Add MIUI Camera Thanks to @lostark13 for the apk

--- msm8953.mk
+++ msm8953.mk
@@ -245,6 +245,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+    
+# MIUI Camera
+PRODUCT_PACKAGES += \
+    MiuiCam
 
 # Netutils
 PRODUCT_PACKAGES += \
